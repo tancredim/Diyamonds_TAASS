2b1d4238e122606762dcff138e7093518245a974
tmatteo7 <tancredimatteo7@gmail.com>
2022-12-09 22:49:36 +0100

    commit da vscode

---
+++ Diyamonds_backend/src/main/java/it/unito/taass/diyamonds/DiyamondsApplication.java
@@ -0,0 +1,15 @@
+package it.unito.taass.diyamonds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("it.unito.taass.diyamonds.model")
+public class DiyamondsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyamondsApplication.class, args);
+		System.out.println("");
+	}
+}
